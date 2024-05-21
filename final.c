5f4616e811226cc80427a2ee492d0c83fff3ae4a
MrChrisJr <mrchrisjr1024@gmail.com>
2024-05-21 13:12:18 -0700

    Added file I/O through processes

--- final.c
+++ final.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-
-#define bool int
-#define false 0
-#define true 1
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
 
 // Structure to hold data for each thread
 typedef struct {
@@ -12,7 +12,7 @@ typedef struct {
     long long result;
 } ThreadData;
 
-//calculates the fibonacci number at value n
+// calculates the fibonacci number at value n
 void *calculate_fibonacci(void *arg) {
     ThreadData *data = (ThreadData *)arg;
     int n = data->n;
@@ -33,7 +33,7 @@ void *calculate_fibonacci(void *arg) {
     pthread_exit(NULL);
 }
 
-//Basic prime checker, checks if a number passed is prime, used in calculate_prime() function
+// Basic prime checker, checks if a number passed is prime, used in calculate_prime() function
 int is_prime(int num) {
     if (num <= 1) return 0;
     if (num <= 3) return 1;
@@ -44,7 +44,7 @@ int is_prime(int num) {
     return 1;
 }
 
-//Function to pass to thread, calculate the prime at value n
+// Function to pass to thread, calculate the prime at value n
 void *calculate_prime(void *arg) {
     ThreadData *data = (ThreadData *)arg;
     int n = data->n;
@@ -60,48 +60,77 @@ void *calculate_prime(void *arg) {
     pthread_exit(NULL);
 }
 
+// Function to handle file I/O in a separate process
+void handle_file_io(const char *filename, int n, long long fib_result, long long prime_result) {
+    FILE *file = fopen(filename, "a");
+    if (file == NULL) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(file, "n: %d, Fibonacci: %lld, Prime: %lld\n", n, fib_result, prime_result);
+    fclose(file);
+}
+
 int main() {
     pthread_t thread1, thread2;
     ThreadData fib_data, prime_data;
-	char arr[50] = "-------- Fibonacci Prime Calculator! --------";
-	char *title = arr;
-	char *temp = title;
-	int i = 0;
-	while(temp[i] != NULL){
-		printf("%c",*(temp+i));
-		i++;
-	}
-	printf("\n");
-	bool c = true;
-	while(c == true){
-		int n;
-		char cont = 'n';
-		printf("Enter the value of n: ");
-		scanf("%d", &n);
+    char arr[50] = "-------- Fibonacci Prime Calculator! --------";
+    char *title = arr;
+    char *temp = title;
+    int i = 0;
+    while (temp[i] != '\0') {
+        printf("%c", *(temp + i));
+        i++;
+    }
+    printf("\n");
 
-		fib_data.n = n;
-		prime_data.n = n;
+    const char *filename = "results.txt";
+    bool c = true;
 
-		// Create threads to calculate Fibonacci and prime numbers
-		pthread_create(&thread1, NULL, calculate_fibonacci, &fib_data);
-		pthread_create(&thread2, NULL, calculate_prime, &prime_data);
+    while (c) {
+        int n;
+        char cont = 'n';
+        printf("Enter the value of n: ");
+        scanf("%d", &n);
 
-		// Wait for both threads to finish
-		pthread_join(thread1, NULL);
-		pthread_join(thread2, NULL);
+        fib_data.n = n;
+        prime_data.n = n;
 
-		// Print results
-		printf("The %dth Fibonacci number is: %lld\n", n, fib_data.result);
-		printf("The %dth prime number is: %lld\n", n, prime_data.result);
-		printf("Would you like to continue? y/n: ");
-		scanf(" %c",&cont);
-		
-		if(cont == 'y'){
-			c = true;
-		}
-		else if(cont == 'n'){
-			c = false;
-		}
-	}
+        // Create threads to calculate Fibonacci and prime numbers
+        pthread_create(&thread1, NULL, calculate_fibonacci, &fib_data);
+        pthread_create(&thread2, NULL, calculate_prime, &prime_data);
+
+        // Wait for both threads to finish
+        pthread_join(thread1, NULL);
+        pthread_join(thread2, NULL);
+
+        // Print results
+        printf("The %dth Fibonacci number is: %lld\n", n, fib_data.result);
+        printf("The %dth prime number is: %lld\n", n, prime_data.result);
+
+        // Create a process to handle file I/O
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("Failed to fork");
+            return EXIT_FAILURE;
+        } else if (pid == 0) {
+            // Child process handles file I/O
+            handle_file_io(filename, n, fib_data.result, prime_data.result);
+            exit(0);
+        } else {
+            // Parent process waits for the child to finish
+            wait(NULL);
+        }
+
+        printf("Would you like to continue? y/n: ");
+        while (getchar() != '\n'); // Clear input buffer
+        scanf(" %c", &cont);
+
+        if (cont == 'y' || cont == 'Y') {
+            c = true;
+        } else if (cont == 'n' || cont == 'N') {
+            c = false;
+        }
+    }
     return 0;
 }
