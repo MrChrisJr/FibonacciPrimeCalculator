7092598811b1cf10e7f2bdc75402d0a239b08d7c
MrChrisJr <mrchrisjr1024@gmail.com>
2024-05-21 12:57:55 -0700

    Added Would you like to continue feature

--- final.c
+++ final.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define bool int
+#define false 0
+#define true 1
+
 // Structure to hold data for each thread
 typedef struct {
     int n;
     long long result;
 } ThreadData;
 
+//calculates the fibonacci number at value n
 void *calculate_fibonacci(void *arg) {
     ThreadData *data = (ThreadData *)arg;
     int n = data->n;
@@ -28,6 +33,7 @@ void *calculate_fibonacci(void *arg) {
     pthread_exit(NULL);
 }
 
+//Basic prime checker, checks if a number passed is prime, used in calculate_prime() function
 int is_prime(int num) {
     if (num <= 1) return 0;
     if (num <= 3) return 1;
@@ -38,6 +44,7 @@ int is_prime(int num) {
     return 1;
 }
 
+//Function to pass to thread, calculate the prime at value n
 void *calculate_prime(void *arg) {
     ThreadData *data = (ThreadData *)arg;
     int n = data->n;
@@ -56,25 +63,45 @@ void *calculate_prime(void *arg) {
 int main() {
     pthread_t thread1, thread2;
     ThreadData fib_data, prime_data;
+	char arr[50] = "-------- Fibonacci Prime Calculator! --------";
+	char *title = arr;
+	char *temp = title;
+	int i = 0;
+	while(temp[i] != NULL){
+		printf("%c",*(temp+i));
+		i++;
+	}
+	printf("\n");
+	bool c = true;
+	while(c == true){
+		int n;
+		char cont = 'n';
+		printf("Enter the value of n: ");
+		scanf("%d", &n);
 
-    int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
+		fib_data.n = n;
+		prime_data.n = n;
 
-    fib_data.n = n;
-    prime_data.n = n;
+		// Create threads to calculate Fibonacci and prime numbers
+		pthread_create(&thread1, NULL, calculate_fibonacci, &fib_data);
+		pthread_create(&thread2, NULL, calculate_prime, &prime_data);
 
-    // Create threads to calculate Fibonacci and prime numbers
-    pthread_create(&thread1, NULL, calculate_fibonacci, &fib_data);
-    pthread_create(&thread2, NULL, calculate_prime, &prime_data);
+		// Wait for both threads to finish
+		pthread_join(thread1, NULL);
+		pthread_join(thread2, NULL);
 
-    // Wait for both threads to finish
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-
-    // Print results
-    printf("The %dth Fibonacci number is: %lld\n", n, fib_data.result);
-    printf("The %dth prime number is: %lld\n", n, prime_data.result);
-
+		// Print results
+		printf("The %dth Fibonacci number is: %lld\n", n, fib_data.result);
+		printf("The %dth prime number is: %lld\n", n, prime_data.result);
+		printf("Would you like to continue? y/n: ");
+		scanf(" %c",&cont);
+		
+		if(cont == 'y'){
+			c = true;
+		}
+		else if(cont == 'n'){
+			c = false;
+		}
+	}
     return 0;
 }
